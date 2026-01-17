9d3c90650d4a0de06028714db1dcd1f18ada1987
Stephen882-pixel <stephenondeyo0@gmail.com>
2026-01-17 16:39:51 +0300

    Newsletter  component

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -393,7 +393,7 @@ const AboutPage = () => {
 
       <div className="py-16">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <Newsletter variant="default" />
+          <NewsLetter variant="default" />
         </div>
       </div>
 
