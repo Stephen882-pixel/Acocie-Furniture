7ff3d86ba4bed008fb58a363532019ed38b70eb5
Stephen882-pixel <stephenondeyo0@gmail.com>
2026-01-17 23:23:59 +0300

    initialize useSatate

---
+++ src/components/product/ReviewForm.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { Star, CheckCircle } from "lucide-react";
+import { useUser } from "../../context/UserContext";
+
+const ReviewForm = ({ productName, onsubmit }) => {
+  const { isAuthenticated } = useUser();
+  const [rating, setRating] = useState(0);
+  const [hoverRating, setHoverRating] = useState(0);
+  const [submitted, setSubmitted] = useState(false);
+  const [formData, setFormData] = useState({
+    title: "",
+    comment: "",
+    recommend: true,
+  });
+
+  
+};
