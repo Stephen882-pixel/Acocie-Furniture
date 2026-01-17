ca983c4e0f7e32a7201be478f66c8d253a6691d5
Stephen882-pixel <stephenondeyo0@gmail.com>
2026-01-17 10:37:49 +0300

    update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import HomePage from './pages/HomePage';
-import { CartProvider } from './context/CartContext'
+import ProductsPage from './pages/ProductsPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
 
 function App() {
-
   return (
     <CartProvider>
-      <div className='min-h-screen flex flex-col'>
-        <Header />
-        <main className='flex-grow'>
-            <HomePage />
-        </main>
-        <Footer />
-      </div>
+      <Router>
+        <div className="min-h-screen flex flex-col">
+          {/* Header - Sticky Navigation */}
+          <Header />
+          
+          {/* Main Content Area */}
+          <main className="flex-grow">
+            <Routes>
+              {/* Home Page */}
+              <Route path="/" element={<HomePage />} />
+              
+              {/* Products Listing Page */}
+              <Route path="/products" element={<ProductsPage />} />
+              
+              {/* Individual Product Detail Page */}
+              <Route path="/product/:id" element={<ProductDetailPage />} />
+              
+              {/* Shopping Cart Page */}
+              <Route path="/cart" element={<CartPage />} />
+              
+              {/* Checkout Page */}
+              <Route path="/checkout" element={<CheckoutPage />} />
+            </Routes>
+          </main>
+          
+          {/* Footer */}
+          <Footer />
+        </div>
+      </Router>
     </CartProvider>
   );
 }
 
-export default App
+export default App;
